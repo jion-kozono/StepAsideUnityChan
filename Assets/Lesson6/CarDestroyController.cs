953f0c129135db2c525e07d3ba4a843f221a2221
jion-kozono <zeon66190329@gmail.com>
2019-10-29 21:02:06 +0900

    課題：不要になったアイテムを順次破棄しましょう 2019_10_29

---
+++ Assets/Lesson6/CarDestroyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarDestroyController : MonoBehaviour
+{
+
+    //Main Cameraのオブジェクト
+    private GameObject maincamera;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Main Cameraのオブジェクトを取得
+        this.maincamera = GameObject.Find("Main Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (maincamera.transform.position.z > this.transform.position.z)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
